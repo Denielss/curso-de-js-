cbe37916872d233a53b9feafcfbcd55afaf51f45
Daniel <daniel.martin@escola.pr.gov.br>
2024-10-04 09:59:39 -0300

    aula 1,4
    mexendo com js tambem

---
+++ aula.js
@@ -0,0 +1,12 @@
+let a = 2;
+let b = 3;
+let c = 3;
+
+let soma = a+b;
+
+if(soma > c){
+    console.log("o valor "+soma+"é maior que o valor "+ c);
+}
+else{
+    console.log("o valor "+soma+"é menor que o valor "+ c);
+}
